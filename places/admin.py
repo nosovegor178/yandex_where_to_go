4a5a022008102518808a1afaaa9af82a0554da58
nosovegor178 <nosovbelakovegor01@gmail.com>
2025-05-16 18:19:35 +0300

    Edited images styles

--- places/admin.py
+++ places/admin.py
@@ -9,7 +9,7 @@ def preview_inline(obj):
     try:
         if obj.image_id:
             image = Image.objects.get(pk=obj.image_id)
-            return format_html('<img src="{}" style="max-height: 200px;">', image.image.url)
+            return format_html('<img src="{}" style="max-width:300px; max-height: 200px;">', image.image.url)
     except Exception as e:
         return format_html('<span style="color: red;">Ошибка загрузки: {}</span>', e)
 
@@ -38,6 +38,6 @@ class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     def preview(obj):
-        return format_html('<img src="{url}" style="width:300px; height:200px;">', url=obj.image.url)
+        return format_html('<img src="{}" style="max-width:300px; max-height:200px;">', obj.image.url)
     list_display = ['title',]
     readonly_fields = [preview,]
