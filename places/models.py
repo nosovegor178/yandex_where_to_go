cdfeea577096099da4bfbdf86acba7a575ce06c5
nosovegor178 <nosovbelakovegor01@gmail.com>
2025-05-27 06:37:38 +0300

    Improved code with flake8

--- places/models.py
+++ places/models.py
@@ -3,14 +3,18 @@ from tinymce import models as tinymce_models
 
 
 class Image(models.Model):
-    place = models.ForeignKey('Place',
-                              on_delete=models.CASCADE,
-                              related_name='images',
-                              verbose_name='Место')
+    place = models.ForeignKey(
+        'Place',
+        on_delete=models.CASCADE,
+        related_name='images',
+        verbose_name='Место'
+        )
     image = models.ImageField(verbose_name='Изображение')
-    images_order = models.PositiveIntegerField(default=0,
-                                               db_index=True,
-                                               verbose_name='Порядковый номер')
+    images_order = models.PositiveIntegerField(
+        default=0,
+        db_index=True,
+        verbose_name='Порядковый номер'
+        )
 
     class Meta:
         ordering = ['images_order']
@@ -23,21 +27,26 @@ class Place(models.Model):
     title = models.CharField(
         max_length=255,
         verbose_name='Название',
-        unique=True)
+        unique=True
+        )
     short_description = models.TextField(
         verbose_name='Краткое описание',
-        blank=True)
+        blank=True
+        )
     long_description = tinymce_models.HTMLField(
         verbose_name='Полное описание',
-        blank=True)
+        blank=True
+        )
     latitude = models.DecimalField(
         verbose_name='Широта',
         max_digits=17,
-        decimal_places=14)
+        decimal_places=14
+        )
     longitude = models.DecimalField(
         verbose_name='Долгота',
         max_digits=17,
-        decimal_places=14)
+        decimal_places=14
+        )
 
     def __str__(self):
         return f'{self.title}'
